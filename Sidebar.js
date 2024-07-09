f4d7f764d401d995ef0b3219c2d63d68332f2ac2
gkeerthi02 <keerthigovindupalli@gmail.com>
2024-07-09 09:16:05 +0530

    bar.js

---
+++ Sidebar.js
@@ -0,0 +1,12 @@
+const Sidebar = () => {
+    return (
+        <div className="sidebabr">
+            <span>
+                category information
+                sidebar 
+            </span>
+        </div>
+      );
+}
+ 
+export default Sidebar;
